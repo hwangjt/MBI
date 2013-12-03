98c48b9b1bc353c6a6721281a2740cfa2dca133b
Bret Naylor <naylor.b@gmail.com>
2013-12-03 16:31:30 -0500

    updated setup.py file

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ kwds = {'install_requires': ['numpy', 'scipy'],
         'zip_safe': False,
         'license': 'LGPL',
         'packages': ['MBI'],
-        'script_args': ['build', '--fcompiler=gnu95', 'install']
+        #'script_args': ['build', '--fcompiler=gnu95', 'install']
         }
 kwds.update(config.todict())
 
